d8157ae8b9da3d205fef8a47118451682b899aa4
brajac709 <bij4@cornell.edu>
2021-04-15 18:41:31 -0400

    Add basic example client from the docs.
    Tested connection, but have not added to a server to test message
    capability

---
+++ discordMusicFilterBot.py
@@ -0,0 +1,24 @@
+import discord
+
+with open('tokenfile.txt','r') as f:
+    token = f.read()
+
+if (token is None):
+    raise RuntimeError('Cannot read token from tokenfile.txt')
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.authoer == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(token)
