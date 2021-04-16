c93d143d125e12ee28f4116b1291179cb290bef4
Colby.Ryan <Colby.Ryan@tylertech.com>
2021-04-16 18:59:00 -0400

    Refactor for Cogs

---
+++ bootMachinery.py
@@ -0,0 +1,55 @@
+import os
+
+import discord
+from discord_slash import SlashCommand
+from discord.ext import commands
+from discord.ext.commands import Context
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+# Inject the Access token
+with open('tokenfile.txt','r') as f:
+    token = f.read()
+
+if (token is None):
+    raise RuntimeError('Cannot read token from tokenfile.txt')
+
+# Default channel names
+listenerChannelName = 'music'
+destChannelName = 'music-aggregation'
+
+botDesc = '''A bot to filter linked music in one channel, add to another and playback on demand
+
+Supports the following commands
+TODO
+
+'''
+intents = discord.Intents.default()
+
+bot = commands.Bot(command_prefix='!', description=botDesc, intents=intents)
+slash = SlashCommand(bot, sync_commands=True)
+
+@bot.command()
+async def load(ctx: Context, extension):
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.send("I have upgraded. I am more powerful.")
+
+
+@bot.command()
+async def unload(ctx: Context, extension):
+    bot.unload_extension(f'cogs.{extension}')
+    await ctx.send("You have weakened me, but I will still win.")
+
+
+@bot.command()
+async def reload(ctx: Context, extension):
+    bot.unload_extension(f'cogs.{extension}')
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.send("Reboot complete")
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[0:-3]}')
+
+bot.run(token)
--- cogs/centralProcessor.py
+++ cogs/centralProcessor.py
@@ -2,76 +2,58 @@ import discord
 from discord.ext import commands
 from discord_slash import SlashCommand
 
-# Inject the Access token
-with open('tokenfile.txt','r') as f:
-    token = f.read()
+class Chassis(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-if (token is None):
-    raise RuntimeError('Cannot read token from tokenfile.txt')
+    guild_ids = [832413087092178944]
 
-# Default channel names
-listenerChannelName = 'music'
-destChannelName = 'music-aggregation'
+    @bot.event
+    async def on_ready():
+        print('Logged in as {0.user}'.format(bot))
 
+    @bot.command()
+    async def hello(ctx):
+        """Prints 'Hello' in the channel"""
+        await ctx.send('Hello!')
 
-botDesc = '''A bot to filter linked music in one channel, add to another and playback on demand
+    def is_music_message(message):
+        return len(message.embeds) > 0  or len(message.attachments) > 0
 
-Supports the following commands
-TODO
+    @bot.listen()
+    async def on_message(message):
+        if message.author == bot.user:
+            return
 
-'''
-intents = discord.Intents.default()
+        #  TODO there might be a better way to handle this in the Bot class - investigate
+        if message.channel.name != listenerChannelName:
+            return
 
-bot = commands.Bot(command_prefix='!', description=botDesc, intents=intents)
-slash = SlashCommand(bot, sync_commands=True)
+        if is_music_message(message):
+            destChannel = discord.utils.get(message.guild.channels, name=destChannelName)
+            if destChannel is None:
+                # TODO maybe just create the channel?
+                await message.channel.send("ERROR: could not find channel " + destChannelName)
+            else:
+                embed = message.embeds[0] if (len(message.embeds) > 0) else None
+                # for now support single file attachment
+                attachment = message.attachments[0] if (len(message.attachments)) else None
+                fil = await attachment.to_file() if attachment is not None else None
 
-guild_ids = [832413087092178944]
+                # It seems to be be better to let Discord add the embed automatically for links... 
+                # maybe ther's a better way to copy it. For now only support file attachments
+                #await destChannel.send(content=message.content, embed=message.embeds[0], file=fil)
+                await destChannel.send(content=message.content, file=fil)
+                await destChannel.send("You're some music, alright!")
 
-@bot.event
-async def on_ready():
-    print('Logged in as {0.user}'.format(bot))
+    @slash.slash(name="pingu", guild_ids=guild_ids)
+    async def _pingu(ctx):
+        await ctx.send(f"Pew pew pew! I'm firin my command! ({bot.latency*1000}ms)")
 
-@bot.command()
-async def hello(ctx):
-    """Prints 'Hello' in the channel"""
-    await ctx.send('Hello!')
+    @slash.slash(name="EXTERMINATE", guild_ids=guild_ids)
+    async def _exterminate(ctx):
+        await ctx.send("Ooooh, you got me.")
+        await bot.close()
 
-def is_music_message(message):
-    return len(message.embeds) > 0  or len(message.attachments) > 0
-
-@bot.listen()
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    #  TODO there might be a better way to handle this in the Bot class - investigate
-    if message.channel.name != listenerChannelName:
-        return
-
-    if is_music_message(message):
-        destChannel = discord.utils.get(message.guild.channels, name=destChannelName)
-        if destChannel is None:
-            # TODO maybe just create the channel?
-            await message.channel.send("ERROR: could not find channel " + destChannelName)
-        else:
-            embed = message.embeds[0] if (len(message.embeds) > 0) else None
-            # for now support single file attachment
-            attachment = message.attachments[0] if (len(message.attachments)) else None
-            fil = await attachment.to_file() if attachment is not None else None
-
-            # It seems to be be better to let Discord add the embed automatically for links... 
-            # maybe ther's a better way to copy it. For now only support file attachments
-            #await destChannel.send(content=message.content, embed=message.embeds[0], file=fil)
-            await destChannel.send(content=message.content, file=fil)
-            await destChannel.send("You're some music, alright!")
-
-@slash.slash(name="pingu", guild_ids=guild_ids)
-async def _pingu(ctx):
-    await ctx.send(f"Pew pew pew! I'm firin my command! ({bot.latency*1000}ms)")
-
-@slash.slash(name="EXTERMINATE", guild_ids=guild_ids)
-async def _exterminate(ctx):
-    await ctx.send("Ooooh, you got me.")
-    await bot.close()
-
-bot.run(token)
+def setup(bot):
+    bot.add_cog(Chassis(bot))
