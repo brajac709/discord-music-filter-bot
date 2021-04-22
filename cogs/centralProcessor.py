82253f49336cd8198c8e6f4e7ec5ad56a43b13cf
brajac709 <bij4@cornell.edu>
2021-04-21 22:26:42 -0400

    try redirecting stdout,stderr
    move to bootMachinery.py later

--- cogs/centralProcessor.py
+++ cogs/centralProcessor.py
@@ -4,6 +4,7 @@ from discord_slash import cog_ext, SlashContext
 import musicDatabase as db
 import json
 import youtube_dl
+import sys, io
 
 listenerChannelName = 'music'
 destChannelName = 'music-aggregation'
@@ -164,4 +165,14 @@ class Chassis(commands.Cog):
         await self.bot.close()
 
 def setup(bot):
+    f = open('logfile.txt','a')
+    f.seek(0,io.SEEK_END)
+    sys.stdout = f
+    sys.stderr = f
     bot.add_cog(Chassis(bot))
+
+def teardown(bot):
+    sys.stdout.close()
+    sys.stderr.close()
+    sys.stdout = sys.__stdout__
+    sys.stderr = sys.__stderr__
